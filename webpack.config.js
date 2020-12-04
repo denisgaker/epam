bb340e8db89b2147ce627c7642f0db2e9c97a9d9
denisgaker <denisgaker@gmail.com>
2020-12-04 18:32:21 +0300

    Task 2 | Development (source maps, wathc mode, dev server)

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     // entry: './src/index.js',
     entry: {
         index: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+            contentBase: path.resolve(__dirname, 'dist'),
+            open: true,
+            compress: true,
+            hot: true,
+            port: 9000,
+        // contentBase: path.resolve(__dirname, 'dist')
+        
+    },
     plugins: [
-        new CleanWebpackPlugin(),
+        // new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
         new HtmlWebpackPlugin({
-            title: 'Output Management'
+            title: 'Development'
         })
     ],
     output: {
