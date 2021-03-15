d1ca24ce105f9d899327ddfa6f3076d953189f2e
denisgaker <denisgaker@gmail.com>
2021-03-15 10:57:16 +0300

    Удалил закомментированный код

--- src/HeaderError.jsx
+++ src/HeaderError.jsx
@@ -2,13 +2,4 @@ import React from 'react'
 
 const HeaderError = (Component) => ({isError, ...props}) => isError ? <h2 className="HeaderError">Шапку потеряли...</h2> : <Component {...props} />;
 
-{/* function WithHeaderError(Component) {
-    const Message = () => <h2 className="HeaderError">Шапку потеряли...</h2>
-
-    return function HeaderErrorComponent({ isHeaderError, ...props }) {
-        if (!isHeaderError) return <Component { ...props }/>
-        return <><Message /></>
-    }
-} */}
-
 export default HeaderError
