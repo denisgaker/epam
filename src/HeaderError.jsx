15b07b3e98bab709f5f659a7a4bd7ec4eb2af4c9
denisgaker <denisgaker@gmail.com>
2021-04-09 17:10:09 +0300

    Обновлены импорты в App.tsx

--- src/HeaderError.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const HeaderError = (Component) => ({isError, ...props}) => isError ? <h2 className="HeaderError">Шапку потеряли...</h2> : <Component {...props} />;
-
-export default HeaderError
