dc17ffa05f8d40d644af54253b2a8f7b806729a6
denisgaker <denisgaker@gmail.com>
2021-04-14 15:31:51 +0300

    Header как React.FC

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import './style.scss'
 
-const Header = (props: any) => <header className="header">{props.children}</header>
+const Header: React.FC = (props) => <header className="header">{props.children}</header>
 
 export default Header
