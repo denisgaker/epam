b459239e095917807851098a3c6597439ee08197
denisgaker <denisgaker@gmail.com>
2021-04-14 15:26:15 +0300

    Footer как React.FC

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import './style.scss'
 
-const Footer = (props: any) => <footer className="footer">{props.children}</footer>
+const Footer: React.FC = (props) => <footer className="footer">{props.children}</footer>
 
 export default Footer
